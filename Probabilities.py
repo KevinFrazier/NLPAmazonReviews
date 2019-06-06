de8dde68f6d7122a395dc9abf26fccd52300342a
KevinFrazier <kfraz001@ucr.edu>
2019-06-05 21:38:39 -0700

    updated probabilities

--- Probabilities.py
+++ Probabilities.py
@@ -5,27 +5,34 @@ from nltk.stem.porter import *
 
 def writeToFile(data, fileName):
 
-    print("data")
-    print(data)
-   
-
     f = open(fileName,"a+")
 
     for eachIndex in data:
-        print(eachIndex)
-        f.write(str(eachIndex) + "\t,\t" + str(data[eachIndex]) + "\n")
+        f.write(str(eachIndex) + "," + str(data[eachIndex]) + "\n")
 
 
     f.close()
 
+def getNgrams(text, n):
+    if n == 1:
+        return text
+    
+    ngramList = []
+    for i in range(len(text) - (n-1)):
+        ngram = text[i:i+n]
+        ngramList.append(ngram)
+    
+    return ngramList
+    
 def main():
     
     stemmer = PorterStemmer()
-    data_directory = "./amazon-fine-food-reviews/Reviews.csv"
+    data_directory = "./Reviews.csv"
 
 
     data = pd.read_csv(data_directory)
 
+    print("starting")
     unigramDict = {}
     bigramDict = {}
     for index, row in data.iterrows():
@@ -38,6 +45,7 @@ def main():
         #calculate unigram
         for eachWord in parsedText:
             eachWord = stemmer.stem(eachWord)
+            eachWord = eachWord.lower()
             if eachWord in unigramDict:
                 unigramDict[eachWord] +=1
             else:
@@ -48,7 +56,7 @@ def main():
         #calculate bigram
 
         for index in range(len(parsedText)-1):
-            bigram = (parsedText[index],parsedText[index+1])
+            bigram = parsedText[index] + "," + parsedText[index+1]
             if bigram in bigramDict:
                 bigramDict[bigram] +=1
             else:
